a924ebf8c4dab6689a0dd6a0da75b18ff91a1e33
bluePlatinum <jukic.rok@gmail.com>
2025-03-25 22:51:28 +0100

    added presharedKey functionality

--- src/WireGuard-ESP32.h
+++ src/WireGuard-ESP32.h
@@ -10,6 +10,7 @@ class WireGuard
 private:
     bool _is_initialized = false;
 public:
+    bool begin(const IPAddress& localIP, const IPAddress& Subnet, const IPAddress& Gateway, const char* privateKey, const char* remotePeerAddress, const char* remotePeerPublicKey, uint16_t remotePeerPort, const char* presharedKey);
     bool begin(const IPAddress& localIP, const IPAddress& Subnet, const IPAddress& Gateway, const char* privateKey, const char* remotePeerAddress, const char* remotePeerPublicKey, uint16_t remotePeerPort);
     bool begin(const IPAddress& localIP, const char* privateKey, const char* remotePeerAddress, const char* remotePeerPublicKey, uint16_t remotePeerPort);
     void end();
